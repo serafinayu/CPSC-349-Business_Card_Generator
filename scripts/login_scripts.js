1a970b5e82c3ef812cdec04a420835f56f3dac2b
An Daoe <daoducthienan007@gmail.com>
2023-10-16 07:00:48 -0700

    check if email or password not enter yet

--- scripts/login_scripts.js
+++ scripts/login_scripts.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     const loginButton = document.querySelector('.login-button');
     const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
     const emailError = document.getElementById('emailError');
+    const passwordError = document.getElementById('passwordError');
 
     loginButton.addEventListener('click', function(event) {
         event.preventDefault();
@@ -9,15 +11,25 @@ document.addEventListener('DOMContentLoaded', function() {
         // Clear previous messages
         emailError.textContent = "";
         emailError.style.display = "none";
+        passwordError.textContent = "";
+        passwordError.style.display = "none";
 
         // Email validation
-        if (validateEmail(emailInput.value)) {
-            storeEmail(emailInput.value);
-            
-        } else {
-            emailError.textContent = "Please enter a valid email.";
+        if (!validateEmail(emailInput.value)) {
+            emailError.textContent = "Please enter a valid email!";
             emailError.style.display = "block";
+            return;
         }
+
+        // Password check
+        if (passwordInput.value.trim() === "") {
+            passwordError.textContent = "Please enter a password!";
+            passwordError.style.display = "block";
+            return;
+        }
+
+        storeEmail(emailInput.value);
+        //code to navigate to another page when successful login
     });
 });
 
