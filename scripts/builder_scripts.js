393513bbfb9221b22bb58364d6f1e9462dfec44f
Serafina Yu <serafyu@csu.fullerton.edu>
2023-10-23 14:52:43 -0700

    Some firebase changes
    Need to complete writing to realtime database

--- scripts/builder_scripts.js
+++ scripts/builder_scripts.js
@@ -1,3 +1,122 @@
-function updateCard(elementId, value) {
-    document.getElementById(`card-${elementId}`).innerText = value;
-}
+// Import the functions you need from the SDKs you need
+// Add SDKs for Firebase products that you want to use
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-analytics.js";
+import { getDatabase, ref, set } from "firebase/database";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAuFs8e9otYS84DrXUnsbDxP59aay2rfI0",
+  authDomain: "business-card-generator-120d7.firebaseapp.com",
+  projectId: "business-card-generator-120d7",
+  storageBucket: "business-card-generator-120d7.appspot.com",
+  messagingSenderId: "230677413050",
+  appId: "1:230677413050:web:58be376af87566665e5683",
+  measurementId: "G-87VKWPXR2B",
+  databaseURL:
+    "https://business-card-generator-120d7-default-rtdb.firebaseio.com/",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getDatabase(app);
+const infoForm = document.getElementById("infoForm");
+
+// Handle form submission
+infoForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  // var name = document.getElementById("name").value;
+  const fname = document.getElementById("firstName").value;
+  const lname = document.getElementById("lastName").value;
+  const github = document.getElementById("github").value;
+  const linkedin = document.getElementById("linkedin").value;
+  const title = document.getElementById("title").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone").value;
+  // const skills = document.getElementById("skills").value;
+
+  // Push data to the database
+  db.ref("users").push({
+    //   name: name,
+    fname: fname,
+    lname: lname,
+    github: github,
+    linkedin: linkedin,
+    title: title,
+    email: email,
+    phone: phone,
+    //   skills: skills
+  });
+
+  alert("Form submitted successfully!");
+
+  // Clear the form
+  infoForm.reset();
+});
+
+// infoForm.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     const fname = document.getElementById('firstName').nodeValue;
+//     const lname = document.getElementById('lastName');
+//     const title = document.getElementById('title');
+//     const githubUrl = document.getElementById('github');
+//     const linkedinUrl = document.getElementById('linkedin');
+//     const email = document.getElementById('email');
+//     const phone = document.getElementById('phont');
+//     const db = getDatabase();
+
+//     // set(ref(db, 'users/' + uid), {
+//     //     fname: fname,
+//     //     lname: lname,
+//     //     title: title,
+//     //     github: githubUrl,
+//     //     linkedin: linkedinUrl,
+//     //     email: email,
+//     //     phone: phone,
+//     //     // profile_picture : imageUrl
+//     // });
+//     // document.location.href = "/../final.html";
+
+//     set(ref(db, 'users/'), {
+//         fname: fname,
+//         lname: lname,
+//         title: title,
+//         github: githubUrl,
+//         linkedin: linkedinUrl,
+//         email: email,
+//         phone: phone,
+//         // profile_picture : imageUrl
+//     });
+//     document.location.href = "/../final.html";
+
+// })
+
+// function writeBusinessCard(uid, firstName, lastName, title, githubUrl, linkedinUrl, email, phone) {
+//     const db = getDatabase();
+//     set(ref(db, 'users/' + uid), {
+//         fname: firstName,
+//         lname: lastName,
+//         title: title,
+//         github: githubUrl,
+//         linkedin: linkedinUrl,
+//         email: email,
+//         phone: phone,
+//         // profile_picture : imageUrl
+//     });
+//     document.location.href = "/../final.html";
+// }
+
+// document.querySelector('#submit').addEventListener('click', ()=>{
+//     const fname = document.getElementById('firstName');
+//     const lname = document.getElementById('lastName');
+//     const title = document.getElementById('title');
+//     const github = document.getElementById('github');
+//     const linkedin = document.getElementById('linkedin');
+//     const email = document.getElementById('email');
+//     const phone = document.getElementById('phont');
+//     firebaseRef.push()
+// })
--- scripts/signup_scripts.js
+++ scripts/signup_scripts.js
@@ -1,65 +1,49 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const joinButton = document.querySelector('.join-button');
-    const nameInput = document.getElementById('name');
-    const emailInput = document.getElementById('email');
-    const passwordInput = document.getElementById('password');
-    const nameError = document.getElementById('nameError');
-    const emailError = document.getElementById('emailError');
-    const passwordError = document.getElementById('passwordError');
+// SDK script to connect to Firebase and its features
+    
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-analytics.js";
+import { getDatabase } from "firebase/database";
+import { getAuth, createUserWithEmailAndPassword, signOut } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-    joinButton.addEventListener('click', function (event) {
-        event.preventDefault();
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAuFs8e9otYS84DrXUnsbDxP59aay2rfI0",
+    authDomain: "business-card-generator-120d7.firebaseapp.com",
+    projectId: "business-card-generator-120d7",
+    storageBucket: "business-card-generator-120d7.appspot.com",
+    messagingSenderId: "230677413050",
+    appId: "1:230677413050:web:58be376af87566665e5683",
+    measurementId: "G-87VKWPXR2B",
+    databaseURL: "https://business-card-generator-120d7-default-rtdb.firebaseio.com/"
+};
 
-        // Clear previous messages
-        nameError.textContent = "";
-        nameError.style.display = "none";
-        emailError.textContent = "";
-        emailError.style.display = "none";
-        passwordError.textContent = "";
-        passwordError.style.display = "none";
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getDatabase(app);
+const auth = getAuth();
 
-        // Name validation
-        if (!nameInput.value.trim()) {
-            nameError.textContent = "Name is required!";
-            nameError.style.display = "block";
-            return;
-        }
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
 
-        // Email validation
-        if (!validateEmail(emailInput.value)) {
-            emailError.textContent = "Invalid email address.";
-            emailError.style.display = "block";
-            return;
-        }
-
-        // Password validation
-        if (passwordInput.value.trim().length < 6) {
-            passwordError.textContent = "Password must be at least 6 characters.";
-            passwordError.style.display = "block";
-            return;
-        }
-
-        storeDetails(nameInput.value, emailInput.value, passwordInput.value);
-        // Code to navigate to another page when successful sign up or clear input values        
-    });
+signOut(auth).then(() => {
+// Sign-out successful.
+}).catch((error) => {
+// An error happened.
 });
 
-function validateEmail(email) {
-    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-    return regex.test(email);
-}
-
-function storeDetails(name, email, password) {
-    localStorage.setItem('name', name);
-    localStorage.setItem('email', email);
-    localStorage.setItem('password', password);
-}
-
-function createErrorMessage(id, inputElement) {
-    const errorMessage = document.createElement('span');
-    errorMessage.id = id;
-    errorMessage.style.color = "red";
-    errorMessage.style.display = "none";
-    inputElement.parentElement.appendChild(errorMessage);
-    return errorMessage;
-}
+const submit = document.getElementById("join-button")
+submit.addEventListener("submit", logSubmit);
