ba5ffc3b72827389abfb1f421d1f55c34ef35b7c
jhytai <josue.tai@csu.fullerton.edu>
2023-10-23 20:55:37 -0700

    Updated final page

--- scripts/final_scripts.js
+++ scripts/final_scripts.js
@@ -2,6 +2,7 @@
 // Add SDKs for Firebase products that you want to use
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js';
 import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.5.0/firebase-analytics.js';
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js";
 import { getDatabase, ref, set, get } from 'https://www.gstatic.com/firebasejs/10.5.0/firebase-database.js';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -40,7 +41,7 @@ const analytics = getAnalytics(app);
 // function displayCardData() {
 //     // Retrieve the data from sessionStorage
 //     let cardData = JSON.parse(sessionStorage.getItem("cardData"));
-
+//
 //     if (cardData) {
 //         // Use the data to populate the business card
 //         document.querySelector(".name").textContent = cardData.firstName + " " + cardData.lastName;
@@ -56,12 +57,12 @@ const analytics = getAnalytics(app);
 //     if (profilePicBase64) {
 //         document.querySelector(".profile-pic").src = profilePicBase64;
 //     }
-
+//
 //     const companyLogoBase64 = sessionStorage.getItem("companyLogoBase64");
 //     if (companyLogoBase64) {
 //         document.querySelector(".company-logo").src = companyLogoBase64;
 //     }
 // }
-
+//
 // // Call the function to display the card data
 // displayCardData();
