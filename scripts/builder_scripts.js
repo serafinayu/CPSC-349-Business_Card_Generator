72715db097bf4421436add8f394623722f3fb6f3
Matthew Sanchez <mattsanchez31993@csu.fullerton.edu>
2023-10-23 17:51:41 -0700

    Added more to the backend
    you can now add company to the business card, and upload company and profile pictures and everything displays

--- scripts/builder_scripts.js
+++ scripts/builder_scripts.js
@@ -1,3 +1,41 @@
-function updateCard(elementId, value) {
-    document.getElementById(`card-${elementId}`).innerText = value;
+function saveImageToSessionStorage(inputElementId, storageKey) {
+    const inputElement = document.getElementById(inputElementId);
+    if (inputElement.files && inputElement.files[0]) {
+        const reader = new FileReader();
+
+        reader.onload = function(event) {
+            // Save the Base64 string to sessionStorage
+            sessionStorage.setItem(storageKey, event.target.result);
+        };
+
+        reader.readAsDataURL(inputElement.files[0]);
+    }
 }
+
+document.getElementById("submit").addEventListener("click", function() {
+    function collectFormData() {
+        // Collecting form data
+        let cardData = {
+            firstName: document.getElementById("firstName").value,
+            lastName: document.getElementById("lastName").value,
+            github: document.getElementById("github").value,
+            linkedin: document.getElementById("linkedin").value,
+            email: document.getElementById("email").value,
+            phone: document.getElementById("phone").value,
+            title: document.getElementById("title").value,
+            skills: document.getElementById("skills").value,
+            company: document.getElementById("company").value,
+        };
+    
+        // Save the collected data to sessionStorage
+        sessionStorage.setItem("cardData", JSON.stringify(cardData));
+
+        // Save the uploaded images to sessionStorage
+        saveImageToSessionStorage("profilePic", "profilePicBase64");
+        saveImageToSessionStorage("companyLogo", "companyLogoBase64");
+    
+        // Redirect to final.html or whatever processing you have
+        window.location.href = "./final.html";
+    }
+    collectFormData();
+});
